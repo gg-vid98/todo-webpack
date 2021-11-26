d41e51c417f5e8a73a69011726abdd3572c86b1b
Vidya Arunachalam <vidya.arunachalam@publicissapient.com>
2021-11-26 11:11:12 +0530

    added redux

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from "react";
+import TodoList from "./todos/TodoList";
+export default function App() {
+  return (
+    <div className="App">
+      <TodoList />
+    </div>
+  );
+}
---
+++ src/store.js
@@ -0,0 +1,21 @@
+import { createStore, combineReducers } from "redux";
+import { todos } from "./todos/reducers";
+import storage from "redux-persist/lib/storage";
+import { persistReducer } from "redux-persist";
+import autoMergeLevel2 from "redux-persist/es/stateReconciler/autoMergeLevel2";
+const reducers = {
+  todos,
+};
+const persistConfig = {
+  key: "root",
+  storage,
+  stateReconciler: autoMergeLevel2,
+};
+
+const rootReducer = combineReducers(reducers);
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+export const configureStore = () =>
+  createStore(
+    persistedReducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+  );
